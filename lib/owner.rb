4963627b27e80fc31037e6917a80da25ce985b8b
Benjamin Small <ben20262@aim.com>
2020-01-06 19:22:40 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -4,8 +4,12 @@ class Owner
 
   def initialize (name)
     @name = name
-    @species = "I am a human."
+    @species = "human"
     @@all << self
+  end
+
+  def say_species
+    puts "I am a #{@species}."
   end
 
   def self.all
