7c2df353ad296001928938522a504612712cff1d
Benjamin Small <ben20262@aim.com>
2020-01-06 19:28:41 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,15 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@all = []
+
+  def initialize (name, owner)
+    @name = name
+    @owner = Owner.all.select {|person| person.name == owner}[0]
+    @mood = "nervous"
+  end
+
+  def self.all
+    @@all
+  end
 end
